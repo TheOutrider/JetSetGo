e4f149272df7c12b2a9d8dcafc918f5c3963870a
TheOutrider <brijeshkumbhare999@gmail.com>
2026-06-04 21:01:43 +0530

    Add new Button styles

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -6,6 +6,14 @@ public class MainMenu : MonoBehaviour
     public GameObject ButtonSidePanel;
     public GameObject MapPanel;
 
+     [SerializeField] private float rotationSpeed = 50f; // Degrees per second
+     [SerializeField] private RectTransform rectTransform ;
+
+    private void Update()
+    {
+        rectTransform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+    }
+
     public void OnTimeLimitClicked()
     {
         GameManager.Instance.SelectTimeLimit();
