ca11bbb005c9741f8b1b2e3d9f8040d96d35fdc7
TheOutrider <brijeshkumbhare999@gmail.com>
2026-06-17 18:49:21 +0530

    Adding AI enemies

---
+++ Assets/Scripts/Game/RacingOffline/RacerInfo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class RacerInfo : MonoBehaviour
+{
+    public string racerName;
+    public bool isPlayer;
+
+    [HideInInspector] public int currentCheckpointIndex = 0;
+    [HideInInspector] public int totalCheckpoints;
+    [HideInInspector] public bool hasFinished;
+    [HideInInspector] public float finishTime;
+
+    void Start()
+    {
+        RaceModeManager.Instance?.RegisterRacer(this);
+    }
+}
